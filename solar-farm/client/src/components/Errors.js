f8b031522fa22f3a4a13bfa0e117ef53cf722aca
James Churchill <james@smashdev.com>
2023-08-09 14:16:26 -0700

    Create starter project

--- solar-farm/client/src/components/Errors.js
+++ solar-farm/client/src/components/Errors.js
@@ -1,8 +1,4 @@
 function Errors({ errors }) {
-  if (!errors || !Array.isArray(errors) || errors.length === 0) {
-    return null;
-  }
-
   return (
     <div>
       <p>The following errors were found:</p>
